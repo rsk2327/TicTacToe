ed33c80739e2d374b33f340e8a16eb06bed8efa0
rsk2327 <roshan.santhosh@gmail.com>
2016-12-30 09:04:30 +0530

    basic files

---
+++ tictactoegame.py
@@ -0,0 +1,54 @@
+
+
+grid3List=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+class tictactoe():
+
+	def __init__(self,gridSize=3):
+
+		self.gridSize=3
+		self.valuesEntered=0
+		self.isActive=True
+
+		self.gridValues=[]
+		for i in range(self.gridSize**2):
+			self.gridValues.append('')
+
+		self.currentPlayer = 1
+
+
+
+
+
+	def gameWon(self):
+		"""Returns whether the game has been won or the game has ended in a draw"""
+
+		if self.gridSize==3:
+
+			for i in range(len(grid3List)):
+				checkList = grid3List[i]
+				val1,val2,val3 = self.gridValues[checkList[0]],self.gridValues[checkList[1]],self.gridValues[checkList[2]]
+				if (val1==val2 and val2==val3 and val1!=""):
+					self.isActive=False		
+					return "W"
+
+			if self.valuesEntered==self.gridSize**2:
+				self.isActive=False
+				return "D"
+			else:
+				return "C"
+
+
+	def gameReset(self):
+		""" Resets the game to original state """
+
+		self.valuesEntered=0
+		self.isActive=True
+		self.currentPlayer=1
+		for i in range(self.gridSize**2):
+			self.gridValues[i]=''
+
+		return None
+
+
+
